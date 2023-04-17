1ffb02f3313420e32d610a4d466c774785c7a807
Narenboi <73293318+Narenboi@users.noreply.github.com>
2023-04-17 12:45:56 +0200

    Add files via upload

---
+++ serving-web-content/serving-web-content/src/main/java/com/example/servingwebcontent/HjemmesideRigtigeApplication.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjemmesideRigtigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjemmesideRigtigeApplication.class, args);
+	}
+
+}
